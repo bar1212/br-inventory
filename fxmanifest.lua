b5704b7e6afce10e2dbe9cdf0f2ee5cd9c06fde7
Bar <75738858+bar1212@users.noreply.github.com>
2023-05-23 17:34:45 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,38 @@
+fx_version 'cerulean'
+game 'gta5'
+Auth "Bar.#9999"
+description 'br-Inventory'
+version '1.0.0'
+
+shared_scripts {
+    '@br-core/shared/locale.lua',
+    'locales/en.lua',
+    'locales/*.lua',
+    'config.lua',
+}
+
+server_scripts {
+	'@oxmysql/lib/MySQL.lua',
+	'server/main.lua'
+}
+
+client_script 'client/main.lua'
+
+ui_page {
+	'html/ui.html'
+}
+
+files {
+	'html/ui.html',
+	'html/css/main.css',
+	'html/js/app.js',
+	'html/images/*.png',
+	'html/images/*.jpg',
+	'html/ammo_images/*.png',
+	'html/attachment_images/*.png',
+	'html/*.ttf'
+}
+
+dependency 'br-weapons'
+
+lua54 'yes'
